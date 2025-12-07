30abe28f26f3707b2d7971edf06509b5fe748ab6
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-12-07 12:32:36 +0100

    Add index page view and update root route to use it

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+<x-guest-layout>
+    <h1 class="text-3xl font-bold underline">
+        Üdv az Atád rendszerben!
+    </h1>
+    <a href="{{ route('login') }}"
+        class="">
+        Log in
+    </a>
+</x-guest-layout>
