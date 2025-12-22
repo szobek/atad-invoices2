783d757685c473430435d0c110c621116e594f3f
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-12-22 15:02:26 +0100

    feat: refactor chart data initialization to handle null chart_bar element

--- resources/js/chart/datas.js
+++ resources/js/chart/datas.js
@@ -1,47 +1,53 @@
 import { Utils } from './utils.js'
 
 const labels = Utils.labels;
-const bar_data = {
-    labels: labels,
-    datasets: [
-        {
-            label: 'számlák száma',
-            data: JSON.parse(chart_bar.dataset.invoices),
-            backgroundColor: Utils.CHART_COLORS.red,
-        },
-        {
-            label: 'sztornó számlák',
-            data: JSON.parse(chart_bar.dataset.storno),
-            backgroundColor: Utils.CHART_COLORS.blue,
-        },
+const chart_bar = document.getElementById('chart_bar') || null;
+let bar_data = {};
+let line_data = {};
+if (chart_bar) {
 
-        {
-            label: 'lorem ipsum',
-            data: JSON.parse(chart_bar.dataset.lorem),
-            backgroundColor: Utils.CHART_COLORS.green,
-        },
+    bar_data = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'számlák száma',
+                data: JSON.parse(chart_bar.dataset.invoices) || "[]",
+                backgroundColor: Utils.CHART_COLORS.red,
+            },
+            {
+                label: 'sztornó számlák',
+                data: JSON.parse(chart_bar.dataset.storno) || "[]",
+                backgroundColor: Utils.CHART_COLORS.blue,
+            },
 
-
-    ]
-};
-const line_data = {
-    labels: labels,
-    datasets: [
-        {
-            label: 'számlák száma',
-            data: JSON.parse(chart_bar.dataset.invoices),
-            backgroundColor: Utils.CHART_COLORS.red,
-        },
-        {
-            label: 'sztornó számlák',
-            data: JSON.parse(chart_bar.dataset.storno),
-            backgroundColor: Utils.CHART_COLORS.blue,
-        },
-
-       
+            {
+                label: 'lorem ipsum',
+                data: JSON.parse(chart_bar.dataset.lorem) || "[]",
+                backgroundColor: Utils.CHART_COLORS.green,
+            },
 
 
-    ]
-};
+        ]
+    };
+    line_data = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'számlák száma',
+                data: JSON.parse(chart_bar.dataset.invoices),
+                backgroundColor: Utils.CHART_COLORS.red,
+            },
+            {
+                label: 'sztornó számlák',
+                data: JSON.parse(chart_bar.dataset.storno),
+                backgroundColor: Utils.CHART_COLORS.blue,
+            },
 
-export { bar_data,line_data }
+
+
+
+        ]
+    };
+
+}
+export { bar_data, line_data }
