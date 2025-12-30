5b6bd075614eec48a15b48666f4fe600bd89f12a
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-12-30 12:51:41 +0100

    feat: Fix conditional rendering for user deletion form in detail view

--- resources/views/pages/user/detail.blade.php
+++ resources/views/pages/user/detail.blade.php
@@ -16,11 +16,13 @@
                 <p><strong>Név:</strong> {{ $user->name }}</p>
                 <p><strong>Email:</strong> {{ $user->email }}</p>
                 <p><strong>Szerepkör:</strong> {{ $user->role }}</p>
+                @if(($user->id != auth()->user()->id)&& ($user->role != 'admin'))
                 <form action="{{ route('user.delete', $user->id) }}" method="post">
                     @csrf
                     @method('DELETE')
                     <button class="btn btn-danger">törlés</button>
                 </form>
+                @endif
             </div>
         </div>
     </div>
