5939ad26959fdb4981da6d8962a1d74e2b7f65e6
Kunszt Norbert <kunszt.norbert@gmail.com>
2026-01-02 12:32:27 +0100

    feat:add translate and modify html on profile page

--- lang/hu/user.php
+++ lang/hu/user.php
@@ -5,4 +5,12 @@ return [
     "sales" => "Értékesítő",
     "salesperson" => "Üzletkötő",
     "role" => "Szerepkör",
+    "update_user_title"=>"Profil információk",
+    "update_user_subtitle"=>"Itt tudod frissíteni az adatokat",
+    "update_user_password_title"=>"Jelszó megváltoztatása",
+    "update_user_password_subtitle"=>"A biztonság megőrzése érdekében győződjön meg arról, hogy fiókja hosszú, véletlenszerű jelszót használ.",
+    "name"=>"Név",
+    "current_password"=>"Jelenlegi jelszó",
+    "new_password"=>"Új jelszó",
+    "confirm_password"=>"Jelszó megerősítés",
 ];
