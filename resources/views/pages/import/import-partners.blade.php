731706ce3c5dc9546a774a314b48b9cc57ebbb48
Kunszt Norbert <kunszt.norbert@gmail.com>
2026-01-02 10:58:50 +0100

    fix: fix html and add a title

--- resources/views/pages/import/import-partners.blade.php
+++ resources/views/pages/import/import-partners.blade.php
@@ -1,11 +1,8 @@
-<x-app-layout>
+<x-app-layout title="Partnerek importálása">
     <div class="container">
         <div class="row">
             <div class="col-12">
                 <h1>Excel fájl feltöltése</h1>
-
-                <
-
                 <form action="{{ route('post.import.partners') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <input type="file" name="file" required class="form-control">
